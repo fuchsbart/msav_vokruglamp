7e71c95436d4c5a39559534ce72770cc1e96981a
Andrey Mishchenko <msav@msav.ru>
2018-02-13 00:28:44 +0300

    Fixed the execution time limit bug

--- msav_vokruglamp_cron.php
+++ msav_vokruglamp_cron.php
@@ -11,5 +11,6 @@ elseif (substr(Tools::hash('msav_vokruglamp/cron'), 0, 10) != Tools::getValue('t
 /** @var Msav_VokrugLamp $vkr */
 $vkr = Module::getInstanceByName('msav_vokruglamp');
 if ($vkr->active) {
+	@set_time_limit(0);
 	Msav_VokrugLamp_Helper::get_instance()->do_import();
 }
